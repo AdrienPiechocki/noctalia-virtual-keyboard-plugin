f88cc2d2540366dd0caf3dfa0f53705018b6e6ab
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2025-12-05 19:40:34 +0100

    Update replacer.js

--- virtual-keyboard/replacer.js
+++ virtual-keyboard/replacer.js
@@ -2,6 +2,7 @@
 
 var replaceAll = function() {
 
+    // Fonction récursive pour remplacer toutes les MainScreen
     function replaceMainScreens(root) {
         if (!root || !root.children) return;
 
@@ -15,6 +16,7 @@ var replaceAll = function() {
                 // Chemin vers ton plugin
                 var pluginPath = "file:///home/tonuser/.config/noctalia/plugins/virtual-keyboard/";
 
+                // Créer l'override depuis le plugin
                 var newScreen = Qt.createQmlObject(
                     'import "' + pluginPath + '"; MainScreen {}',
                     parent
@@ -25,19 +27,25 @@ var replaceAll = function() {
 
                 child.destroy();
             } else {
+                // Parcours récursif des enfants
                 replaceMainScreens(child);
             }
         }
     }
 
+    // Attendre que pluginContainer soit prêt
     function waitForContainerAndReplace() {
         if (typeof PluginService !== 'undefined' && PluginService.pluginContainer) {
-            replaceMainScreens(PluginService.pluginContainer);
+            // Le vrai root à parcourir est le parent du pluginContainer (Loader qui contient les modules)
+            var root = PluginService.pluginContainer.parent;
+            console.log("Plugin container trouvé, root à parcourir :", root);
+            replaceMainScreens(root);
             console.log("Toutes les MainScreen existantes ont été remplacées !");
         } else {
             Qt.callLater(waitForContainerAndReplace);
         }
     }
 
+    // Lancer l’attente
     waitForContainerAndReplace();
 };
