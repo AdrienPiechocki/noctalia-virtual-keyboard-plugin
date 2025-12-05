dec1810190b810bdfcbfeb18a155e29538d0c74a
AdrienPiechocki <112864425+AdrienPiechocki@users.noreply.github.com>
2025-12-05 19:33:52 +0100

    Update replacer.js

--- virtual-keyboard/replacer.js
+++ virtual-keyboard/replacer.js
@@ -1,20 +1,27 @@
 // ReplaceMainScreen.js
-var ReplacementScript = {};
 
-ReplacementScript.replaceAll = function() {
+var replaceAll = function() {
+
     function replaceMainScreens(root) {
         if (!root || !root.children) return;
+
         for (var i = root.children.length - 1; i >= 0; i--) {
             var child = root.children[i];
+
             if (child.constructor && child.constructor.name === "MainScreen") {
                 var parent = child.parent;
+
+                // Chemin vers ton plugin
                 var pluginPath = "file:///home/tonuser/.config/noctalia/plugins/virtual-keyboard/";
+
                 var newScreen = Qt.createQmlObject(
                     'import "' + pluginPath + '"; MainScreen {}',
                     parent
                 );
+
                 if ("width" in child) newScreen.width = child.width;
                 if ("height" in child) newScreen.height = child.height;
+
                 child.destroy();
             } else {
                 replaceMainScreens(child);
